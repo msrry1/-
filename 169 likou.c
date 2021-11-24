ac52c51eb9312ce377f4bff7bed883d70e0a2d32
msrry1 <2105348162@qq.com>
2021-11-24 18:15:52 +0800

    力扣169.多数元素

---
+++ 169 likou.c
@@ -0,0 +1,8 @@
+int inc(const void *a,const void *b)
+{
+    return *(int*)a - *(int*)b;
+}
+int majorityElement(int* nums, int numsSize){
+    qsort(nums,numsSize,sizeof(int),inc);
+    return nums[numsSize / 2];
+}
